39ca6044c7962b4293ce42d8736049067e4d09b1
Ingram Chen <ingramchen@gmail.com>
2015-01-16 21:00:52 +0800

    basic web application configuration

---
+++ kaif-web/src/main/java/io/kaif/web/IndexController.java
@@ -0,0 +1,14 @@
+package io.kaif.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index";
+  }
+
+}
