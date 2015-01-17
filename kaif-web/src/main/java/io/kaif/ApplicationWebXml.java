b5656892d09eda66aa022398be2c56468b5cb603
Ingram Chen <ingramchen@gmail.com>
2015-01-17 23:29:16 +0800

    make deployable war for spring boot

---
+++ kaif-web/src/main/java/io/kaif/ApplicationWebXml.java
@@ -0,0 +1,20 @@
+package io.kaif;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * This is an helper Java class that provides an alternative to creating a web.xml.
+ */
+public class ApplicationWebXml extends SpringBootServletInitializer {
+
+  private final Logger log = LoggerFactory.getLogger(ApplicationWebXml.class);
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+
+}
