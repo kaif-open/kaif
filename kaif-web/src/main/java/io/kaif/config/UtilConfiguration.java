c5cd53580f1320c669690a31553bf9c855d7dba6
kojilin <koji.lin@gmail.com>
2015-01-19 14:23:21 +0800

    fix misspelled word configuration

--- kaif-web/src/main/java/io/kaif/config/UtilConfiguration.java
+++ kaif-web/src/main/java/io/kaif/config/UtilConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class UtilConfigration {
+public class UtilConfiguration {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
--- kaif-web/src/test/java/io/kaif/database/DbIntegrationTests.java
+++ kaif-web/src/test/java/io/kaif/database/DbIntegrationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 
 import io.kaif.config.SpringProfile;
-import io.kaif.config.UtilConfigration;
+import io.kaif.config.UtilConfiguration;
 
 @ActiveProfiles(SpringProfile.TEST)
 @SpringApplicationConfiguration(classes = DbIntegrationTests.JdbcTestApplication.class)
@@ -17,7 +17,7 @@ public abstract class DbIntegrationTests extends AbstractTransactionalJUnit4Spri
 
   @ComponentScan(basePackages = "io.kaif.model")
   @Import(value = { DataSourceAutoConfiguration.class,
-      DataSourceTransactionManagerAutoConfiguration.class, UtilConfigration.class })
+      DataSourceTransactionManagerAutoConfiguration.class, UtilConfiguration.class })
   public static class JdbcTestApplication {
 
   }
