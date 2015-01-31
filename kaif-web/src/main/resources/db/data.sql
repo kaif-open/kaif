43af57fbadf661712ef2f87139b941a0ecb2ea70
Ingram Chen <ingramchen@gmail.com>
2015-01-31 19:44:24 +0800

    data.sql patch

--- kaif-web/src/main/resources/db/data.sql
+++ kaif-web/src/main/resources/db/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO ZoneInfo (zone, aliasname, theme, readauthority, writeauthority,
-                      adminaccountids, createtime)
-VALUES ('programming', '程設', 'z-theme-default', 'TOURIST', 'CITIZEN', '{}', now());
+INSERT INTO ZoneInfo (zone, aliasname, theme, voteauthority, writeauthority, adminaccountids,
+                      hidefromtop, allowdownvote, createtime)
+VALUES ('programming', '程設', 'z-theme-default', 'CITIZEN', 'CITIZEN', '{}', FALSE, TRUE, now());
 
-INSERT INTO ZoneInfo (zone, aliasname, theme, readauthority, writeauthority,
-                      adminaccountids, createtime)
-VALUES ('kaif-faq', 'Kaif 常見問題', 'z-theme-kaif', 'TOURIST', 'ZONE_ADMIN', '{}', now());
+INSERT INTO ZoneInfo (zone, aliasname, theme, voteauthority, writeauthority, adminaccountids,
+                      hidefromtop, allowdownvote, createtime)
+VALUES ('kaif-faq', 'Kaif 常見問題', 'z-theme-kaif', 'CITIZEN', 'SYSOP', '{}', TRUE, TRUE, now());
