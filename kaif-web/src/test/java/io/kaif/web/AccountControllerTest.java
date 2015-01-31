dc741664dc1370e115776856a08e4dcf054dde37
Ingram Chen <ingramchen@gmail.com>
2015-01-31 23:55:55 +0800

    basic Zone/Account controller mvc test

---
+++ kaif-web/src/test/java/io/kaif/web/AccountControllerTest.java
@@ -0,0 +1,38 @@
+package io.kaif.web;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.time.Instant;
+import java.util.EnumSet;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.Test;
+
+import io.kaif.model.account.Account;
+import io.kaif.model.account.AccountAccessToken;
+import io.kaif.model.account.Authority;
+
+public class AccountControllerTest extends MvcIntegrationTests {
+
+  @Test
+  public void settingsPart() throws Exception {
+    AccountAccessToken token = new AccountAccessToken(UUID.randomUUID(),
+        "pw",
+        EnumSet.allOf(Authority.class));
+    when(accountService.tryDecodeAccessToken("a-token")).thenReturn(Optional.of(token));
+
+    Account account = Account.create("fooName", "foo@gmail.com", "pw", Instant.now());
+
+    when(accountService.findById(token.getAccountId())).thenReturn(Optional.of(account));
+
+    mockMvc.perform(get("/account/settings.part").header("X-KAIF-ACCESS-TOKEN", "a-token"))
+        .andExpect(view().name("account/settings.part"))
+        .andExpect(content().string(containsString("foo@gmail.com")))
+        .andExpect(content().string(containsString("fooName")));
+  }
+}
