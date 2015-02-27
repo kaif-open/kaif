11755412a4d035fed76489817bdb45f8e7763adb
Ingram Chen <ingramchen@gmail.com>
2015-02-27 23:53:07 +0800

    hide access denied exception stacktrace in production

--- kaif-web/src/main/java/io/kaif/web/api/RestExceptionHandler.java
+++ kaif-web/src/main/java/io/kaif/web/api/RestExceptionHandler.java
@@ -1,5 +1,7 @@
 package io.kaif.web.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import io.kaif.config.SpringProfile;
 import io.kaif.model.exception.DomainException;
 import io.kaif.web.support.AbstractRestExceptionHandler;
 import io.kaif.web.support.AccessDeniedException;
@@ -42,6 +45,9 @@ public class RestExceptionHandler extends AbstractRestExceptionHandler {
     }
   }
 
+  @Autowired
+  private Environment environment;
+
   @ExceptionHandler(AccessDeniedException.class)
   @ResponseBody
   public ResponseEntity<RestErrorResponse> handleRestAccessDeniedException(final AccessDeniedException ex,
@@ -49,7 +55,12 @@ public class RestExceptionHandler extends AbstractRestExceptionHandler {
     final HttpStatus status = HttpStatus.UNAUTHORIZED;
     final RestErrorResponse errorResponse = new RestErrorResponse(status.value(),
         i18n(request, "rest-error.RestAccessDeniedException"));
-    logException(ex, errorResponse, request);
+    if (environment.acceptsProfiles(SpringProfile.DEV)) {
+      //only dev server log detail access denied
+      logException(ex, errorResponse, request);
+    } else {
+      logger.warn("{} {}", guessUri(request), ex.getClass().getSimpleName());
+    }
     return new ResponseEntity<>(errorResponse, status);
   }
 
@@ -60,13 +71,17 @@ public class RestExceptionHandler extends AbstractRestExceptionHandler {
     final HttpStatus status = HttpStatus.BAD_REQUEST;
     final TranslatedRestErrorResponse errorResponse = new TranslatedRestErrorResponse(status.value(),
         i18n(request, ex.i18nKey(), ex.i18nArgs().toArray()));
+    //note that domain exception do not use detail log
+    logger.warn("{} {}", guessUri(request), ex.getClass().getSimpleName());
+
+    return new ResponseEntity<>(errorResponse, status);
+  }
+
+  private String guessUri(WebRequest request) {
     String uri = "non uri";
     if (request instanceof ServletWebRequest) {
       uri = ((ServletWebRequest) request).getRequest().getRequestURI();
     }
-    //note that domain exception do not use detail log
-    logger.warn("{} {}", uri, ex.getClass().getSimpleName());
-
-    return new ResponseEntity<>(errorResponse, status);
+    return uri;
   }
 }
