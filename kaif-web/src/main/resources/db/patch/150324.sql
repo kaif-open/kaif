fdd1f87beb949fa4553604a18326469146d68998
kojilin <koji.lin@gmail.com>
2015-04-13 00:13:17 +0800

    patch production admin to ZoneAdmin

--- kaif-web/src/main/resources/db/patch/150324.sql
+++ kaif-web/src/main/resources/db/patch/150324.sql
@@ -1,6 +1,18 @@
 CREATE TABLE ZoneAdmin (
-  accountId       UUID          NOT NULL,
-  zone            VARCHAR(4096) NOT NULL,
-  createTime      TIMESTAMPTZ NOT NULL,
+  accountId  UUID          NOT NULL,
+  zone       VARCHAR(4096) NOT NULL,
+  createTime TIMESTAMPTZ   NOT NULL,
   PRIMARY KEY (accountId, zone)
 );
+
+BEGIN;
+
+INSERT INTO ZoneAdmin (accountId, zone, createTime)
+  SELECT
+    unnest(adminaccountids),
+    zone,
+    createTime,
+    FALSE
+  FROM ZoneInfo;
+
+COMMIT;
