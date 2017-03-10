db7c5cee82d297ebf426eb7657ed9a997f9e375b
Ingram Chen <ingramchen@gmail.com>
2017-03-10 12:18:57 +0800

    upgrade spring to 1.5.2

--- kaif-web/src/main/java/io/kaif/ApplicationWebXml.java
+++ kaif-web/src/main/java/io/kaif/ApplicationWebXml.java
@@ -3,7 +3,7 @@ package io.kaif;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * This is an helper Java class that provides an alternative to creating a web.xml.
