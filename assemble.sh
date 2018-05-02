c2301e537c6ddf6d0f7d7c5aca69800ea2032a8d
Ingram Chen <ingramchen@gmail.com>
2018-05-02 22:13:31 +0800

    build and deploy with open jdk 8

--- assemble.sh
+++ assemble.sh
@@ -2,4 +2,6 @@
 
 CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-$CUR_DIR/gradlew clean assemble
+$CUR_DIR/gradlew clean assemble \
+  -Dorg.gradle.java.home=/Users/ingram/develop/jdk-all/adoptopenjdk/jdk8u144-b01
+
--- test.sh
+++ test.sh
@@ -2,4 +2,6 @@
 
 CUR_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-$CUR_DIR/gradlew clean test
+$CUR_DIR/gradlew clean test \
+  -Dorg.gradle.java.home=/Users/ingram/develop/jdk-all/adoptopenjdk/jdk8u144-b01 
+
