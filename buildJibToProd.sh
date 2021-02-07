9d29e30e287a139673d48067e85e602ee7a4e8f7
Ingram Chen <ingramchen@gmail.com>
2021-02-07 12:50:40 +0800

    gradle jib push to production container registry (google)

---
+++ buildJibToProd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CUR_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  jhome=$(dirname /usr/lib/jvm/*11*/bin)
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  jhome=$(/usr/libexec/java_home -v 11)
+else
+  echo "no java 11 found"
+  exit 1
+fi
+
+JAVA_HOME=$jhome "$CUR_DIR"/gradlew clean jib \
+  -Djib-json-key-file="$CUR_DIR/kaif-deploy/secret/kaif-id-328c723761f5.json"
